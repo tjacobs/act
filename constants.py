36240e0f6e34aa8bd917dae3486e9290bcdf1c64
Tom Jacobs <hatsmagee@gmail.com>
2024-07-04 20:24:09 +0000

    50

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ DATA_DIR = 'data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
-        'num_episodes': 2,
+        'num_episodes': 50,
         'episode_len': 400,
         'camera_names': ['top']
     },
