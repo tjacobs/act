6f7b8dd90f800d2bb1f32e98da91b873393e87d4
Tom Jacobs <hatsmagee@gmail.com>
2024-07-08 21:41:37 -0400

    Update policy.py

--- policy.py
+++ policy.py
@@ -41,33 +41,6 @@ class ACTPolicy(nn.Module):
         return self.optimizer
 
 
-class CNNMLPPolicy(nn.Module):
-    def __init__(self, args_override):
-        super().__init__()
-        model, optimizer = build_CNNMLP_model_and_optimizer(args_override)
-        self.model = model # decoder
-        self.optimizer = optimizer
-
-    def __call__(self, qpos, image, actions=None, is_pad=None):
-        env_state = None # TODO
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-        image = normalize(image)
-        if actions is not None: # training time
-            actions = actions[:, 0]
-            a_hat = self.model(qpos, image, env_state, actions)
-            mse = F.mse_loss(actions, a_hat)
-            loss_dict = dict()
-            loss_dict['mse'] = mse
-            loss_dict['loss'] = loss_dict['mse']
-            return loss_dict
-        else: # inference time
-            a_hat = self.model(qpos, image, env_state) # no action, sample from prior
-            return a_hat
-
-    def configure_optimizers(self):
-        return self.optimizer
-
 def kl_divergence(mu, logvar):
     batch_size = mu.size(0)
     assert batch_size != 0
